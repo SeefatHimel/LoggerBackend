d88cac122186a625e4eafed28a45259bb47ff4b2
Abdul Ahad <abdul.ahad@brainstation-23.com>
2023-02-09 16:11:49 +0600

    added register and login functionalities

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,25 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+// import { User } from '@prisma/client';
 import * as argon from 'argon2';
+import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { LoginDto, RegisterDto } from './dto';
 
 @Injectable()
 export class UsersService {
-  constructor(private prisma: PrismaService, private config: ConfigService) {}
+  constructor(
+    private prisma: PrismaService,
+    private config: ConfigService,
+    private jwt: JwtService,
+  ) {}
 
   async register(dto: RegisterDto) {
     const hashedPassword = await argon.hash(dto.password);
-    let data = {
+
+    const data = {
       email: dto.email,
       firstName: dto.firstName,
       lastName: dto.lastName,
       hash: hashedPassword,
     };
-    const user = await this.prisma.user.create({ data });
+    const user = await this.prisma.user.create({
+      data,
+      select: {
+        id: true,
+        email: true,
+        firstName: true,
+        lastName: true,
+      },
+    });
+    const token = await this.createToken(user);
+    return { ...user, ...token };
   }
 
-  async login(loginDto: LoginDto) {
-    return { ...loginDto };
+  async login(dto: LoginDto) {
+    const user = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    });
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+    const isPasswordMatched = await argon.verify(user.hash, dto.password);
+    if (!isPasswordMatched) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    return await this.createToken(user);
+  }
+
+  async createToken(user: any): Promise<{ access_token: string }> {
+    const payload = { email: user.email, sub: user.id };
+    const access_token = await this.jwt.signAsync(payload, {
+      secret: this.config.get('JWT_SECRET'),
+      expiresIn: '1d',
+    });
+    return {
+      access_token,
+    };
   }
 }
