c0f4c2e9aaf69a29c7d51c0dee1f55b20f0e7797
Abdul Ahad <abdul.ahad@brainstation-23.com>
2023-03-07 13:03:40 +0600

    added worklog api

--- src/sessions/sessions.service.ts
+++ src/sessions/sessions.service.ts
@@ -1,15 +1,28 @@
+import { HttpService } from '@nestjs/axios';
 import {
   BadRequestException,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
-import { SessionStatus, Status, User } from '@prisma/client';
+import { ConfigService } from '@nestjs/config';
+import {
+  IntegrationType,
+  Session,
+  SessionStatus,
+  Status,
+  User,
+} from '@prisma/client';
+import { lastValueFrom } from 'rxjs';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { SessionDto } from './dto';
 
 @Injectable()
 export class SessionsService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private config: ConfigService,
+    private prisma: PrismaService,
+    private httpService: HttpService,
+  ) {}
 
   async getSessions(user: User, taskId: number) {
     await this.validateTaskAccess(user, taskId);
@@ -50,7 +63,9 @@ export class SessionsService {
       throw new BadRequestException('No active session');
     }
 
-    return await this.stopSessionUtil(activeSession.id);
+    const updated_session = await this.stopSessionUtil(activeSession.id);
+    await this.logToIntegrations(user.id, taskId, updated_session);
+    return updated_session;
   }
 
   async validateTaskAccess(user: User, taskId: number) {
@@ -70,5 +85,79 @@ export class SessionsService {
       where: { id: sessionId },
       data: { endTime: new Date(), status: SessionStatus.STOPPED },
     });
+  }
+
+  async logToIntegrations(userId: number, taskId: number, session: Session) {
+    if (session.endTime == null) {
+      return;
+    }
+    const timeSpent =
+      session.endTime.getSeconds() - session.startTime.getSeconds();
+
+    const jiraIntegration = await this.prisma.integration.findFirst({
+      where: {
+        type: IntegrationType.JIRA,
+        userId: userId,
+      },
+    });
+
+    if (!jiraIntegration) {
+      return;
+    }
+
+    const taskIntegrations = await this.prisma.taskIntegration.findMany({
+      where: {
+        taskId,
+      },
+    });
+
+    console.log(taskIntegrations);
+
+    const headers: any = { 'Content-Type': 'application/json' };
+    const data = {
+      grant_type: 'refresh_token',
+      client_id: this.config.get('JIRA_CLIENT_ID'),
+      client_secret: this.config.get('JIRA_SECRET_KEY'),
+      refresh_token: jiraIntegration.refreshToken,
+    };
+    const tokenUrl = 'https://auth.atlassian.com/oauth/token';
+
+    const tokenResp = (
+      await lastValueFrom(this.httpService.post(tokenUrl, data, headers))
+    ).data;
+
+    const updated_integration = await this.prisma.integration.update({
+      where: { id: jiraIntegration.id },
+      data: {
+        accessToken: tokenResp.access_token,
+        refreshToken: tokenResp.refresh_token,
+      },
+    });
+
+    headers['Authorization'] = `Bearer ${updated_integration.accessToken}`;
+
+    taskIntegrations.forEach(async (taskIntegration) => {
+      const worklogUrl = `https://api.atlassian.com/ex/jira/${jiraIntegration.siteId}/rest/api/3/issue/${taskIntegration.integratedTaskId}/worklog`;
+      const bodyData = {
+        comment: `I did some work from ${session.startTime} to ${session.endTime}`,
+        started: session.startTime.toISOString(),
+        timeSpentSeconds: timeSpent,
+      };
+      console.log(jiraIntegration.accessToken);
+      console.log(worklogUrl);
+      console.log(bodyData);
+
+      const worklogResp = (
+        await lastValueFrom(
+          this.httpService.post(worklogUrl, bodyData, headers),
+        )
+      ).data;
+      console.log(worklogResp);
+
+      if (worklogResp.status == 201) {
+        console.log('successfully updated');
+      }
+    });
+    return { success: true, msg: 'successfully updated to jira' };
   }
 }
